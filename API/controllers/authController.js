fb55380c6cd8a1958cff927edb3e370627f92f2d
MarksonLDjomie <11211050@student.itk.ac.id>
2024-03-14 12:55:02 +0800

    update fitur log

--- API/controllers/authController.js
+++ API/controllers/authController.js
@@ -1,4 +1,5 @@
 const { User } = require("../models/index.js");
+const { logs } = require("../models/index.js")
 
 const register = async (req, res) => {
   const { fullname, email, password } = req.body;
@@ -11,14 +12,21 @@ const register = async (req, res) => {
       role: "PENGGUNA",
       saldoElektronik: 0,
     });
-
+    if (newUser) {
+      const newUserLog = await logs.create({
+        pesan: `New user with ID ${newUser.id_user} registers`,
+        waktu: Date.now(),
+      });
+    } else {
+      message : error.message
+    };
     return res.status(200).json({
-      message: "User Created",
+      message: `User Created`,
       data: newUser,
     });
   } catch (error) {
     return res.status(400).json({
-      message: error.message, // Menggunakan pesan kesalahan yang lebih informatif
+      message: error.message, 
     });
   }
 };
@@ -32,7 +40,10 @@ const login = async (req, res) => {
         email,
       },
     });
-    console.log(user);
+    const userLog = await logs.create({
+      pesan : `User with ID ${user.id_user} logs in`,
+      waktu : Date.now()
+    })
     return res.status(200).json({
       msg: "Berhasil login!",
       user,
